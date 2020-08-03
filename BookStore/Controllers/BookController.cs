b03854fdb40fd7244ed6a5c1bf3330f46af4878d
tanzim abir <55651997+tanzim-abir@users.noreply.github.com>
2020-08-03 08:01:45 +0600

    lib excluded

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string bookName, string bookAuthor)
+        {
+            return _bookRepository.SearchBook(bookName, bookAuthor);
+        }
+
+    }
+}
---
+++ BookStore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class BookRepository
+    {
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public List<BookModel> SearchBook(string bookName, string bookAuthor)
+        {
+            return DataSource().Where(x => x.Title.Contains(bookName) || x.Author.Contains(bookAuthor)).ToList();
+        }
+
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>
+            {
+                new BookModel(){Id = 1, Title = "C#", Author = "microsoft"},
+                new BookModel(){Id = 2, Title = "Java", Author = "oracle"},
+                new BookModel(){Id = 3, Title = "Kotlin", Author = "google"},
+                new BookModel(){Id = 4, Title = "golang", Author = "google"}
+            };
+        }
+    }
+}
